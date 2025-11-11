a2c1bb167a4c3dfe0d04537650f50824b6a8ae62
Mourya Abhay Amarjeet <mouryaabhay2426@gmail.com>
2025-11-11 22:02:58 +0530

    feat: initialize query from URL in ResourcesSection; implement URL update on debounced query change; enhance Searchbar with scrollToId prop

--- src/pages/home/resource-section.jsx
+++ src/pages/home/resource-section.jsx
@@ -1,19 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ResourcesList from "@/components/resources/resources-list";
 import Searchbar from "@/components/other/searchbar";
 import { useDebounce } from "@/hooks/use-debounce";
-import { DEBOUNCE_DELAY_MS } from "@/constant/global";
 
 export default function ResourcesSection() {
-  const [query, setQuery] = useState("");
-  const debouncedQuery = useDebounce(query, DEBOUNCE_DELAY_MS);
+  // Initialize query from URL
+  const params = new URLSearchParams(window.location.search);
+  const initialQuery = params.get("q") || "";
+  const [query, setQuery] = useState(initialQuery);
+
+  // Debounced query
+  const debouncedQuery = useDebounce(query, 300);
+
+  // Update URL whenever debouncedQuery changes
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+
+    if (debouncedQuery) {
+      params.set("q", debouncedQuery);
+      window.history.replaceState({}, "", `${window.location.pathname}?${params.toString()}`);
+    } else {
+      // Remove ? if query is empty
+      if (params.has("q")) {
+        params.delete("q");
+        const newUrl = params.toString()
+          ? `${window.location.pathname}?${params.toString()}`
+          : window.location.pathname;
+        window.history.replaceState({}, "", newUrl);
+      }
+    }
+  }, [debouncedQuery]);
 
   return (
     <div
       id="resources-section"
       className="flex flex-col gap-4 my-6 mx-6 lg:mx-24"
     >
-      <Searchbar value={query} onChange={setQuery} />
+      <Searchbar value={query} onChange={setQuery} scrollToId="resource-section" />
       <ResourcesList query={debouncedQuery} />
     </div>
   );
